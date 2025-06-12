5328b04ae120bf7c22fde65283f6aae12217e387
LanhNe <thanhlanhlit@gmail.com>
2025-06-12 18:09:52 +0700

    [Ordering.API-service] Build Application layer

---
+++ src/Services/Ordering.Application/Common/Behaviours/PerformanceBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using Common.Logging;
+using MediatR;
+using Serilog;
+using System.Diagnostics;
+using ILogger = Serilog.ILogger;
+
+
+namespace Ordering.Application.Common.Behaviours;
+
+public class PerformanceBehaviour<TRequest, TResponse> :
+    IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+{
+    private readonly Stopwatch _timer;
+    private readonly ILogger _logger;
+
+    public PerformanceBehaviour()
+    {
+        _timer = new Stopwatch();
+        _logger = Log.ForContext<PerformanceBehaviour<TRequest, TResponse>>();
+    }
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        _timer.Start();
+        var response = await next();
+        _timer.Stop();
+
+        var elapsedMilliseconds = _timer.ElapsedMilliseconds;
+
+        if (elapsedMilliseconds <= 500) return response;
+
+        var requestName = typeof(TRequest).Name;
+        _logger.Warn($"Application Long Running Request: {requestName} ({elapsedMilliseconds} milliseconds) {request}");
+
+        return response;
+    }
+}    
---
+++ src/Services/Ordering.Application/Common/Behaviours/UnhandledExceptionBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Serilog;
+using ILogger = Serilog.ILogger;
+
+namespace Ordering.Application.Common.Behaviours;
+
+public class UnhandledExceptionBehaviour<TRequest, TResponse> :
+    IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+{
+    private readonly ILogger _logger;
+
+    public UnhandledExceptionBehaviour()
+    {
+        _logger = Log.ForContext<UnhandledExceptionBehaviour<TRequest, TResponse>>();
+    }
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        try
+        {
+            return await next(cancellationToken);
+        }
+        catch (Exception ex)
+        {
+            var requestName = typeof(TRequest).Name;
+            _logger.Error(ex, "Application Request: Unhandled Exception for Request {Name} {@Request}", requestName, request);
+            throw;
+        }
+    }
+}
