4120a37b8de15b0a6f901c128385c2b6d776120c
LanhNe <thanhlanhlit@gmail.com>
2025-12-31 05:48:44 +0700

    [Basket.API] Update cart model

--- src/Services/Basket.API/Entities/Cart.cs
+++ src/Services/Basket.API/Entities/Cart.cs
@@ -2,7 +2,7 @@
 
 public class Cart
 {
-    public string UserName { get; set; }
+    public string UserName { get; set; } = string.Empty;
     public List<CartItem> Items { get; set; } = [];
 
     public Cart()
